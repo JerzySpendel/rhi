655febbb3cf30f04fd57093495ccb3ed50c2d760
Jerzy Spendel <jspendel@gmail.com>
2022-06-07 14:55:30 +0100

    Move solution of task A to a separate module

--- core.py
+++ core.py
@@ -1,10 +1,5 @@
-import math
-
-import requests
 import dataclasses
-import csv
-
-data = requests.get("https://api.le-systeme-solaire.net/rest/bodies").json()
+import math
 
 
 @dataclasses.dataclass
@@ -63,27 +58,5 @@ class Sky:
     def get_planet(self, name: str) -> Planet | None:
         return next((planet for planet in self.planets if planet.name == name))
 
-    def to_csv(self):
-        file = open('planets.csv', 'w', newline='')
-        fieldnames = ['name', 'moons_count', 'smallest_moon_mass', 'second_smallest_moon_mass', 'biggest_moon_mass', 'polar_radius']
-        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-        csv_writer.writeheader()
-        for planet in self.planets:
-            row = {
-                'name': planet.name,
-                'moons_count': len(planet.moons),
-                'polar_radius': planet.polar_radius / 1609.344
-            }
-
-            moons_masses = [moon.mass for moon in planet.moons]
-            smallest_masses = iter(sorted(moons_masses))
-            row['smallest_moon_mass'] = next(smallest_masses, None)
-            row['second_smallest_moon_mass'] = next(smallest_masses, None)
-            row['biggest_moon_mass'] = next(iter(sorted(moons_masses, reverse=True)), None)
-
-            csv_writer.writerow(row)
-
-
-sky = Sky.from_raw(data)
-sky.to_csv()
+    def get_planets(self) -> list[Planet]:
+        return self.planets
